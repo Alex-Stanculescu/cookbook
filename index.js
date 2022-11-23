d8fc71c9214840e9527845869ee7186de4920518
cele <cele@cele.com>
2022-11-23 16:46:00 +0100

    added button

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
+
+const myFunction = () => {
+  alert("Enjoy your meal🧁");
+};
